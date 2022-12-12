f175382e352214eaaea1ffc2617d2e52a61ed5c2
hanae.moubarika@gmail.com <hanae.moubarika@gmail.com>
2022-12-12 21:38:30 +0100

    feat (api) : add entities and mapping
    We create entities for the project

---
+++ server/src/main/java/com/boutiques/server/commons/enums/EJour.java
@@ -0,0 +1,15 @@
+package com.boutiques.server.commons.enums;
+
+/**
+ * Cette énumeration concerne les jours
+ * d'ouverture de la boutique
+ */
+public enum EJour {
+    LUNDI,
+    MARDI,
+    MERCREDI,
+    JEUDI,
+    VENDREDI,
+    SAMEDI,
+    DIMANCHE
+}
---
+++ server/src/main/java/com/boutiques/server/entities/Ouverture.java
@@ -0,0 +1,25 @@
+package com.boutiques.server.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Time;
+
+@Entity
+public class Ouverture {
+
+    @Id
+    @Getter
+    @Setter
+    private OuvertureId ouvertureId;
+
+    @Getter
+    @Setter
+    private Time horaireOuverture;
+
+    @Getter
+    @Setter
+    private Time horaireFermeture;
+}
