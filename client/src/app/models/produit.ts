7afe224c6fcb7133ba4107f79cfe51ca4956b0d2
Dihia AZZEDINE <dihia.azzedine@bluesoft-group.com>
2022-12-18 23:57:38 +0100

    models: boutique, categorie, produit

---
+++ client/src/app/models/produit.ts
@@ -0,0 +1,11 @@
+import { Categorie } from "./categorie";
+
+export interface Produit {
+    id : number;
+    nom : string;
+    description: string;
+    prix: number;
+    quantite: number;
+    categorie : Categorie;
+
+}
